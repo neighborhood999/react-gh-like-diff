0e255cc639e6760162cf9a30a6cbc5d245f8b241
Peng Jie <bivinity.pengzjie@gmail.com>
2017-08-14 13:57:29 +0800

    :sparkles: Diff Component with recompose
    :heavy_plus_sign: diff2html

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { diffHelper, defaultOptions } from './utils';
+import {
+  compose,
+  defaultProps,
+  mapProps,
+  onlyUpdateForPropTypes,
+  setPropTypes
+} from 'recompose';
+
+const renderDiffResult = ({ genDiffHTML }) =>
+  <div dangerouslySetInnerHTML={{ __html: genDiffHTML }} />;
+
+export default compose(
+  defaultProps({ options: defaultOptions }),
+  onlyUpdateForPropTypes,
+  setPropTypes({
+    past: PropTypes.any.isRequired,
+    current: PropTypes.any.isRequired
+  }),
+  mapProps(props => ({
+    genDiffHTML: diffHelper(props)
+  }))
+)(renderDiffResult);
