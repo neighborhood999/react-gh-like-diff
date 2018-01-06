979c343653f91458939936eeaaa1a8f41e3fe54d
Peng Jie <bivinity.pengzjie@gmail.com>
2018-01-06 17:27:04 +0800

    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,17 +5,18 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import css from 'rollup-plugin-css-porter';
 
+const exportsOpt = 'named';
+const globalsOpt = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+  recompose: 'recompose',
+  difflib: 'difflib',
+  util: 'util'
+};
+
 const config = {
   input: 'src/index.js',
   external: ['react', 'prop-types', 'recompose', 'difflib'],
-  globals: {
-    react: 'React',
-    'prop-types': 'PropTypes',
-    recompose: 'recompose',
-    difflib: 'difflib',
-    util: 'util'
-  },
-  exports: 'named',
   plugins: [
     babel({
       exclude: 'node_modules/**',
@@ -31,12 +32,19 @@ const config = {
     css({ dest: 'lib/diff2html.css' })
   ],
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', exports: exportsOpt, globals: globalsOpt },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: exportsOpt,
+      globals: globalsOpt
+    },
     {
       file: pkg.umd,
       format: 'umd',
-      name: 'react-gh-like-diff'
+      name: 'react-gh-like-diff',
+      exports: exportsOpt,
+      globals: globalsOpt
     }
   ]
 };
