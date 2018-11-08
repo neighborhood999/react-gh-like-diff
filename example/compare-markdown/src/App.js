8036c571f661dbe2e2f1f71521cdb7cf6f1a96d7
Peng Jie <dean.leehom@gmail.com>
2018-11-08 12:37:24 +0800

    update example (#92)

---
+++ example/compare-markdown/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { ReactGhLikeDiff } from 'react-gh-like-diff';
+import fetch from 'unfetch';
+import { TITLE, PAST, CURRENT } from './constants';
+import './App.css';
+import 'react-gh-like-diff/lib/diff2html.min.css';
+
+class App extends Component {
+  state = {
+    past: '',
+    current: ''
+  };
+
+  componentDidMount() {
+    fetch(PAST)
+    .then(response => response.text())
+    .then(past => this.setState({ past }));
+
+    fetch(CURRENT)
+    .then(response => response.text())
+    .then(current => this.setState({ current }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ReactGhLikeDiff
+          options={{
+            originalFileName: TITLE,
+            updatedFileName: TITLE,
+          }}
+          past={this.state.past}
+          current={this.state.current}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ example/compare-markdown/src/constants.js
@@ -0,0 +1,3 @@
+export const TITLE = 'Algebraic-Effects-in-JavaScript-part-1.md';
+export const CURRENT = 'https://gist.githubusercontent.com/yelouafi/57825fdd223e5337ba0cd2b6ed757f53/raw/09453589bf8ecf8d175b3da4ceda5a62511ba7c0/algebraic-effects-series-1.md';
+export const PAST = 'https://gist.githubusercontent.com/yelouafi/57825fdd223e5337ba0cd2b6ed757f53/raw/3b43b2069f83926c952553bb7609d4b9d6fc8544/algebraic-effects-series-1.md';
